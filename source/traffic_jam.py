d62fe696b9306480b74ae9741595c61191d7af1e
Carl Jones <c.m.jones@dur.ac.uk>
2019-03-12 13:59:38 +0100

    Road running with simple car

--- source/traffic_jam.py
+++ source/traffic_jam.py
@@ -1,3 +1,19 @@
 #!/usr/bin/env python
+from road import Road
 
 ''' Main script to run the traffic jam simulation. '''
+
+n_cars = 10
+n_timesteps = 100
+starting_positions = range(0, 5)
+starting_velocity = 1
+
+road = Road()
+
+# Populate the road with cars
+road.add_multiple_cars(starting_positions, starting_velocity)
+
+road.run_simulation(n_timesteps)
+history_array = road.get_history_position_array()
+
+print(history_array)
