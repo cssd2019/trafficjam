e4b5da0d8f3fcffa3b17c2ad5e62a07dcbd70768
Carl Jones <c.m.jones@dur.ac.uk>
2019-03-12 14:53:26 +0100

    Generate simple test data for plotting

--- source/traffic_jam.py
+++ source/traffic_jam.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python
 from road import Road
 import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
 
 ''' Main script to run the traffic jam simulation. '''
 
-n_cars = 4
+n_cars = 5
 n_timesteps = 30
-starting_positions = range(0, n_cars)
-starting_velocity = 3
+starting_positions = np.linspace(0, 9, n_cars)
+starting_velocity = 2
 
 road = Road()
 
@@ -19,6 +22,8 @@ history_position_array = road.get_history_position_array()
 
 # Save the history array to file
 distance_dataframe = pd.DataFrame(history_position_array)
-# distance_dataframe.to_csv(save_location)
 
-print(distance_dataframe)
+save_location = 'simpleDistanceHistory.csv'
+distance_values = distance_dataframe[0:n_timesteps]
+distance_dataframe.to_csv(save_location)
+
